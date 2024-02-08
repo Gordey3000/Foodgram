f4b73d72ff520b9be334fa8291378c47eaa89e40
Гордей Савичев <gordey159@gmail.com>
2024-02-08 21:36:45 +0300

    2.2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
-
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 INSTALLED_APPS = [
     'django.contrib.admin',
